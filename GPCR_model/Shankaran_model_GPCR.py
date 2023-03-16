868af337a171f3cc3a8776b5b76f4a9133f6725c
Ara101 <94928825+Ara101@users.noreply.github.com>
2023-03-16 16:15:28 -0700

    Update Shankaran_model_GPCR.py

--- GPCR_model/Shankaran_model_GPCR.py
+++ GPCR_model/Shankaran_model_GPCR.py
@@ -1,10 +1,11 @@
 """Recapitulating the model: Receptor downregulation 
 and desensitization enhance the information processing ability of signalling receptors"""
-
+# %%
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import tellurium as te
+import numpy as np 
 
 # The intial EGFR Model
 shankaran_model_EGFR = te.loada("""
@@ -34,100 +35,167 @@ V = 0.0000000004
 
 shankaran_model_EGFR.reset()
 m = shankaran_model_EGFR.simulate(0, 1000, 100)
+plt.figure(1) 
 shankaran_model_EGFR.plot()
 
 # change a species value to recreate the graph
 shankaran_model_EGFR.resetAll()
 count = 0
-plt.figure(1) 
+plt.figure(2) 
 for i in [1,2,5,7.6,10,20]: 
     shankaran_model_EGFR.resetToOrigin()
     shankaran_model_EGFR.ke = 0.15*i
     result = shankaran_model_EGFR.simulate(0, 50, 100,['time','Surface_complex'])
     plt.plot(result[:,0],result[:,1], label = '%s ke = '%i) 
+    plt.title('EGFR w varied ke')
     count += 1
 
 plt.legend()
 plt.show()
-print(count)
 
-# Manipulating the EGFR model 
 
-shankaran_model_EGFR.resetAll()
-count = 0
-plt.figure(2)
-for i in [1,2,5,7.6,10,20]: 
-    shankaran_model_EGFR.resetToOrigin()
-    shankaran_model_EGFR.ke = 0.15*i
-    result = shankaran_model_EGFR.simulate(0, 120, 100,['time','Surface_complex'])
-    plt.plot(result[:,0],result[:,1], label = '%s ke = '%i) 
-    count += 1
 
-plt.legend()
-plt.show()
-print(count)
-
-# %%
 #The GPCR Model
+# seperate ligand and receptor GPCR
 shankaran_model_GPCR = te.loada(""" 
-Ligand + receptor -> Surface_complex; Ligand*receptor*kon
-Surface_complex -> Ligand + receptor; Surface_complex*koff
+$L + $R -> C; L*R*kon
+C -> R; C*koff
+C -> L; koff*C
 
-Surface_complex -> Surface_complex_actived; Surface_complex*kfr
-Surface_complex_actived -> Surface_complex; Surface_complex_actived*krr
+C -> Ca; C*kfr
+Ca -> C; Ca*krr
 
-Surface_complex_actived -> Surface_complex_degraded; Surface_complex_actived*kds
+Ca -> Cd; Ca*kds
 
-Surface_complex_actived -> 2Active_Gprotein; Surface_complex_actived*ka
+Ga -> G; Ga*ki
+G -> Ga; G*ka
+Ca + G -> Ga; ka*G*Ca
 
-Active_Gprotein -> Inactive_Gprotein; Active_Gprotein*ki
-Inactive_Gprotein -> Active_Gprotein; Inactive_Gprotein*ka
+R = RT
+G = GT
+C = 0
+Ca = 0
+Cd =0
+Ga = 0
 
-Ligand = 0.01*Kd + 10612 
-receptor = RT
-Surface_complex = 0
-Active_Gprotein = 100000
-Inactive_Gprotein = Active_Gprotein
-
-kon = 84000000
+kon = 8.4*10^7
 koff = 0.37
 Kd = koff/kon
 kfr = 10
 krr = 10
 kds = 0.065
-ka = 0.0000001
-ki = 0.2
+ka = 10^-7
+ki = 2*10^-1
 
-V = 0.0000000004
-RT = 55000 
-GT = 100000
+Nav = 6.022*10^23
+V = 4*10^-10
+RT = (5.5*10^4)
+GT = (1*10^5)
+
+at (time > 1): $L = 0.01*Kd
 """)
-
 shankaran_model_GPCR.resetToOrigin()
-GPCR_model = shankaran_model_GPCR.simulate(0,120,100)
+GPCR_model = shankaran_model_GPCR.simulate(0,250,100)
+plt.figure(3) 
 shankaran_model_GPCR.plot()
 
 shankaran_model_GPCR.resetAll()
-plt.figure(3)
+plt.figure(4) 
 for i in [10**-2, 10**-1, 10**0, 10**1, 10**2]:
     shankaran_model_GPCR.resetToOrigin()
     shankaran_model_GPCR.kds = 0.065*i
-    result = shankaran_model_GPCR.simulate(0, 250, 200,['time','Active_Gprotein'])
-    plt.plot(result[:,0],result[:,1], label = '%s kds = ' %i)
+    result = shankaran_model_GPCR.simulate(0, 250, 200,['time','Ga'])
+    print(max(result[:,1]))
+    print("Kds = ", shankaran_model_GPCR.kds)
+    plt.plot(result[:,0],result[:,1]*1000, label = '%s kds = ' %i)
+    plt.title('GPCR with varied kds')
 
 plt.legend()
 plt.show()
 
-# Recreating the figures from the study
+# %%
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tellurium as te
+import numpy as np 
+from scipy.integrate import odeint
+#The GPCR Model with differential eq.
+# seperate ligand and receptor GPCR
 
-shankaran_model_GPCR.resetAll()
-plt.figure(4)
-for i in [10**-2, 10**-1, 10**0, 10**1, 10**2]:
-    shankaran_model_GPCR.resetToOrigin()
-    shankaran_model_GPCR.kds = 0.065*i
-    result = shankaran_model_GPCR.simulate(0, 250, 200,['time','Active_Gprotein'])
-    plt.plot(result[:,0],result[:,1], label = '%s kds = ' %i)
+# funtction that returns dX/dt
+#The GPCR Model with differential eq.
+# seperate ligand and receptor GPCR
 
-plt.legend()
+# funtction that returns dX/dt
+def model(X, t):
+    
+    # parameters
+    kon = 8.4e7
+    koff = 0.37
+    Kd = koff/kon
+    kfr = 10
+    krr = 10
+    kds = 0.065
+    ka = 1e-7
+    ki = 2e-1
+    Nav = 6.022e23
+    V = 4e-10
+
+    # mathematical form of the model
+    R = X[0]
+    L = X[1]
+    C = X[2]
+    Ca = X[3]
+    Cd = X[4]
+    G = X[5]
+    Ga = X[6]
+    ft = 0 # or  0.01*Kd
+
+    dRdt = -koff*R*L + koff*C
+    dLdt = (-koff*R*L + koff*C)/(Nav*V) + ft
+    dCdt = koff*R*L - koff*C - kfr*C + krr*Ca
+    dCadt = kfr*C - krr*Ca - kds*Ca
+    dCddt = kds*Ca
+    dGdt = -ka*G*Ca + ki*Ga
+    dGadt = ka*G*Ca - ki*Ga
+
+    return [dRdt, dLdt, dCdt, dCadt, dCddt, dGdt, dGadt]
+
+# Inital conidtions
+
+Kd = 4.404761904761905e-09
+ft = 0.01*Kd
+
+RT = (5.5e4)
+GT = (1e5)
+R = RT
+G = GT
+L = ft
+C = 0
+Ca = 0
+Cd = 0
+Ga = 0
+IC = [R, L, C, Ca, Cd, G, Ga]
+
+# simulation time points
+t = np.linspace(0,250,100)
+
+# solve ODE
+results = odeint(model, IC, t)
+
+plt.figure(5)
+plt.plot(t,results)
+plt.xlabel('time')
+plt.ylabel('Concentation')
+plt.legend(['R', 'L', 'C', 'Ca', 'Cd', 'G', 'Ga'])
+plt.title('GPCR with ODE solver')
 plt.show()
 
+plt.figure(6)
+species = IC.index(Ga)
+plt.plot(t,results[:,species])
+plt.xlabel('time')
+plt.ylabel('Concentation')
+plt.title('GPCR w ODE solver and Ga focus')
+plt.show()
