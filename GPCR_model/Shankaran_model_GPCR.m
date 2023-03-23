e84b0d1b852015ceefa96109c19df048cd20ea77
Ara101 <94928825+Ara101@users.noreply.github.com>
2023-03-23 12:37:50 -0700

    Update Shankaran_model_GPCR.m

--- GPCR_model/Shankaran_model_GPCR.m
+++ GPCR_model/Shankaran_model_GPCR.m
@@ -49,29 +49,22 @@ ylim([0 inf])
 legend('R','L','C','Ca','Cd','G','Ga')
 xlabel('time')
 ylabel('Concentation')
-title('ODE solver with local funtion call')
-
-% % Plot both on the same graph
-% figure(2);subplot(2,1,1)
-% xlabel('time'); ylabel('Concentation')
-% plot(time,X,LineWidth=2); legend('R','L','C','Ca','Cd','G','Ga')
-% subplot(2,1,2)
-% plot(time,X(:,7),LineWidth=2); legend('Species of interest')
+title('Complete GPCR Model with local funtion call')
 
 % Plotting the Ga component of the model
-figure(3)
+figure(2)
 hold on
 for i = [1e-2 1e-1 1e-0 1e1 1e2]
     kds = 0.065*i;
     [time,X] = ode15s(dVdt,time,[R;L;C;Ca;Cd;G;Ga]);
     store = (sprintf('kds = %.1f',kds));
     plot(time,X(:,7)/1000,'DisplayName',store,LineWidth=2)
-    ylim([0 inf])
 end
 hold off
 legend show
-xlabel('time')
-ylabel('Concentation')
+ylim([0 inf])
+xlabel('time(s)')
+ylabel('Activate G-protein Concentation')
 title('Ga species over time w local function call')
 
 
@@ -82,43 +75,52 @@ L = 0.01*Kd;
 ft = A*Kd*koff;
 
 % Plot of Model
-[t,Val] = ode45(@(t,Val) dXdt(t,Val,kds),time,[R;L;C;Ca;Cd;G;Ga]);
-figure(4)
+kds = 0.065; % desensitization rate
+[time,Val] = ode45(@(t,Val) dXdt(t,Val,kds),time,[R;L;C;Ca;Cd;G;Ga]);
+figure(3)
 plot(time,Val,LineWidth=2) 
 ylim([0 inf])
-legend('R','L','C','Ca','Cd','G','Ga')
-xlabel('time')
-ylabel('Concentation')
+legend( 'R','L','C','Ca','Cd','G','Ga')
+title('Complete GPCR Model')
+xlabel('time(s)')
+ylabel('Concentations')
 
-% Using the ODE stiff solver
-figure(5)
-[t,Val] = ode15s(@(t,Val) dXdt(t,Val,kds) ,time, [R;L;C;Ca;Cd;G;Ga]);
-plot(time,Val(:,7)/1000,LineWidth=2)
+% Reproducing the graph Using the ODE stiff solver
+
+figure(4)
+hold on
+for multiple = [1e-2 1e-1 1e-0 1e1 1e2]
+    k_ds = 0.065*multiple;
+    [time,Val] = ode15s(@(t,Val) dXdt(t,Val,k_ds),time, [R;L;C;Ca;Cd;G;Ga]);
+    store = (sprintf('kds = %.1f',k_ds));
+    plot(time,Val(:,7)/1000,'DisplayName',store,LineWidth=2)
+    
+end
+hold off
 ylim([0 inf])
-legend('Ga')
-xlabel('time')
-ylabel('Concentation')
+legend show
+title('Ga species with change in desensitization')
+xlabel('time(s)')
+ylabel('Activate G-protein Concentation')
+
 
 %% ODE user defined funtion
 function xdot = dXdt(t,X,kds)
-%
 
 % parameters
 kon = 8.4e7;
 koff = 0.37;
+Kd = koff/kon;
 kfr = 10;
 krr = 10;
-
 ka = 1e-7;
 ki = 2e-1;
-V = 4e-10;
-Kd = koff/kon;
 Nav = 6.022e23;
-
-ft = 0;%0.01*Kd;
+V = 4e-10;
+ft = 0.01*Kd;
 
 xdot(1) = (-kon*X(1).*X(2)) + koff*X(3);
-xdot(2) = (-kon*X(1).*X(2) + koff*X(3))*(1/(Nav*V)) + ft;
+xdot(2) = (-kon*X(1).*X(2) + koff*X(3))*(1/(Nav*V)) + ft*0;
 xdot(3) = kon*X(1).*X(2) - koff*X(3) - kfr*X(3) + krr*X(4);
 xdot(4) = kfr*X(3) - krr*X(4) - kds*X(4);
 xdot(5) = kds*X(4);
